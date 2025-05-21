5906a8798c373cbe47d2cbee2b8609a4a72c343a
CodingBuddy25 <eksnerhills@outlook.com>
2025-05-21 21:39:05 +0200

    thoughts on passing parameters throughout the flow

--- dinsdag/main_flow.py
+++ dinsdag/main_flow.py
@@ -9,6 +9,24 @@ import asyncio
 
 class ResearchCrewFlow(Flow):
     #the class Flow gets passed, try to use this for passing variables
+    # This is from the other branch, possibly something like this will work?
+    # focus = str(
+    #     input("What is the focus of your research? e.g. bottlenecks, financial risks, IT and Cyber risks, audit risk"))
+    # company = str(input("What is the company your research? e.g. Google, IKEA, proctor and gamble (P&G)"))
+    # process = str(input(
+    #     "What is the process your research should analyse? e.g. loan application, purchase-to-pay, IT incident handling, order to cash"))
+    # filename = str(input("What is the filename (stored int he Event_logs folder) do you want to analyse? e.g. O2C.csv"))
+    # prompt_DK_company = f'Conduct a thorough research about {company}  using the context of {process} if you have it. Use the tavily_search_company and make sure that the research has many references.'
+    # prompt_DK_process = f'Conduct a thorough research about the {process} process using the context of {company} if you have it. Use the tavily_search_process and make sure that the research has many references.'
+    # inputs = {
+    #              'process': process,
+    #              'company': company,
+    #              'focus': focus,
+    #              'chosen_approach': 'DFG',
+    #              'filename': filename,
+    #              'prompt_DK_company': prompt_DK_company,
+    #              'prompt_DK_process': prompt_DK_process
+    # }
     @start()
     def PM_agent(self):
         PM_agent()
