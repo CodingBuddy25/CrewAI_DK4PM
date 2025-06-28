8f5da93f385fef4b8d600bc050a7a34b13c11efc
CodingBuddy25 <eksnerhills@outlook.com>
2025-06-28 12:54:47 +0200

    crew defintion

--- dinsdag/src/dinsdag/crew.py
+++ dinsdag/src/dinsdag/crew.py
@@ -111,7 +111,7 @@ class Dinsdag():
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
+            # process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            # process=Process.hierarchical, # In case you want use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
